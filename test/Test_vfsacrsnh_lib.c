07ac1674c84291048c0171b386e9a2d364c2fd08
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-11-06 21:34:13 -0300

    Unit test of the signal function #17

---
+++ test/Test_vfsacrsnh_lib.c
@@ -0,0 +1,35 @@
+/*
+* Test_vfsacrsnh.c (C)
+* 
+* Purpose: Test of the functions in the library of the program 'Mvfsacrsnh.c'.
+* 
+* Site: https://dirack.github.io
+* 
+* Programer: Rodolfo A C Neves (Dirack) 06/11/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include "Unity/unity.h"
+#include "../vfsacrsnh_lib.h"
+#include <rsf.h>
+
+void setUp(){
+
+};
+
+void tearDown(){};
+
+void test_signal_function(){
+
+	TEST_ASSERT_FLOAT_WITHIN(0.01,1.0,signal(1.0));
+	TEST_ASSERT_FLOAT_WITHIN(0.01,-1.0,signal(-1.0));
+}
+
+int main(void){
+	UNITY_BEGIN();
+	RUN_TEST(test_signal_function);
+	return UNITY_END();
+}
