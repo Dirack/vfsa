09fb56d4248005a041c13be55025402ad69bf001
Dirack <rodolfo_profissional@hotmail.com>
2024-02-03 15:48:16 -0300

    [chore](Release message): Script to autogenerate release message

--- infra/scripts/prepare_release_msg.sh
+++ infra/scripts/prepare_release_msg.sh
@@ -5,24 +5,42 @@
 CURRENTVERSION=$(git branch | grep develop | cut -d"/" -f2)
 LATESTVERSION=$(git describe --abbrev=0)
 GITLOG=$(git log "$LATESTVERSION...develop/$CURRENTVERSION" --oneline)
+GITLOG=$(git log "$LATESTVERSION...master" --oneline)
 
 echo "$CURRENTVERSION - [Summary]"
+echo ""
 echo "[Description]"
+echo ""
 
 echo "## Added"
-echo $(echo "$GITLOG" | grep "^(feat|test)")
+echo "$(echo "$GITLOG" | grep -E "^\[(feat|test)\]" | sed 's/^/- /')"
+echo ""
 
 echo "## Changed"
-echo $(echo "$GITLOG" | grep "^(perf|docs)")
+echo "$(echo "$GITLOG" | grep -E "^\[(perf|docs)\]" | sed 's/^/- /')"
+echo ""
 
 echo "## Fixed"
-echo $(echo "$GITLOG" | grep "^(fix)")
+echo "$(echo "$GITLOG" | grep -E "^\[(fix)\]" | sed 's/^/- /')"
+echo ""
 
 echo "## Removed"
-echo $(echo "$GITLOG" | grep "^(revert)")
+echo "$(echo "$GITLOG" | grep -E "^\[(revert)\]" | sed 's/^/- /')"
+echo ""
 
 echo "## Development"
-echo $(echo "$GITLOG" | grep "^(build|chore|refactor|style)")
+echo "$(echo "$GITLOG" | grep -E "\[(build|chore|refactor|style)\]" | sed 's/^/- /')"
+echo ""
 
-echo "Try it on a docker container!"
-echo "- Not available"
+echo "## Try it on a docker container!"
+echo ""
+echo "From root directory of this repository run to build the image:"
+echo "\`\`\`sh"
+echo "docker build -t vfsa:${CURRENTVERSION} -f ./infra/docker/Dockerfile ."
+echo "\`\`\`sh"
+echo ""
+
+echo "To run it on a docker container use:"
+echo "\`\`\`sh"
+echo "docker run -ti vfsa:${CURRENTVERSION}"
+echo "\`\`\`sh"
