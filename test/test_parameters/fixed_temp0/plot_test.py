4afab33e26bdcd4e4c3013be93bba31c7f08389b
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2024-02-07 20:01:10 +0000

    [WIP] Generate graphs for parameters tests using Madagascar
    Use Madagascar to generate surfaces from tests you do not need python

--- test/test_parameters/fixed_temp0/plot_test.py
+++ test/test_parameters/fixed_temp0/plot_test.py
@@ -1,7 +1,4 @@
 import subprocess
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import numpy as np
 
 oc0=0.
 dc0=0.033
@@ -40,26 +37,6 @@ for nti in range(nnt):
         lc0.append(float(lista[0]))
         lnt.append(float(lista[1]))
         lsemb.append(float(lista[3]))
-        #print(str(c0)+' '+str(temp0)+' '+result)
-        
-#fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-# Creating figure
-fig = plt.figure(figsize =(14, 9))
-ax = plt.axes(projection ='3d')
-# Plot the surface.
-print(lsemb)
-x=np.arange(oc0,1.023,dc0)
-y=np.arange(ont,nnt*dnt+ont,dnt)
-z=np.array(lsemb).reshape(nc0,nnt)
-surf = plt.contour(x,y,z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-# Plot the surface.
-#surf = ax.plot_surface(x, y, z)
-# Creating plot
-#surf = ax.plot_surface(x, y, z,
-#                      edgecolor ='none')
+        print(float(lista[3]))
 
-fig.colorbar(surf, ax = ax,
-             shrink = 0.5, aspect = 5)
-
-ax.set_title('Surface plot')
-plt.show()
+print("n1=%d d1=%g o1=%g label1=c0 n2=%d d2=%g o2=%g label2=Iterations data_format=ascii_float in=surface.asc" % (nc0,dc0,oc0,nnt,dnt,ont))
