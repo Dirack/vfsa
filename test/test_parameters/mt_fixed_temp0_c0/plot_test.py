bad0528d87c9de72b4ffdb2ce34b9f58e9781403
Dirack <rodolfo_profissional@hotmail.com>
2024-02-09 13:03:32 -0300

    [feat](Parameters test): Add multi thread convergence surface
    Add a multi thread convergence surface test to show how semblance varies
    with number of threads and iterations

---
+++ test/test_parameters/mt_fixed_temp0_c0/plot_test.py
@@ -0,0 +1,42 @@
+import subprocess
+
+orep=0
+drep=1
+nrep=10
+
+ont=0
+dnt=1
+nnt=200
+
+om0=5
+dm0=0.1
+nm0=1
+ot0=1.1
+dt0=0.1
+nt0=1
+v0=1.5
+
+lc0=[]
+lnt=[]
+lrn=[]
+lrnip=[]
+lbeta=[]
+lsemb=[]
+
+for nti in range(nnt):
+
+
+    nt = ont+dnt*nti
+    for nrepi in range(nrep):
+
+        repeat = orep+drep*nrepi
+
+        fileinput = 'crsParameters_rep_%d_nt_%d.rsf' % (nrepi,nti)
+        result = subprocess.check_output(['sfdisfil <'+fileinput+' number=n format="%f "'], shell=True)
+        lista = result.split()
+        lc0.append(float(lista[0]))
+        lnt.append(float(lista[1]))
+        lsemb.append(float(lista[3]))
+        print(float(lista[3]))
+
+print("n1=%d d1=%g o1=%g label1=Threads n2=%d d2=%g o2=%g label2=Iterations data_format=ascii_float in=surface.asc" % (nrep,drep,orep,nnt,dnt,ont))
